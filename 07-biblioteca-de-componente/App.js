6732f74c4a48c637fd769c22ab0f4196c1141de0
VitorMod3sto <vitorebrunomodesto@gmail.com>
2025-04-04 21:09:05 -0300

    lista-jogadores

--- 07-biblioteca-de-componente/App.js
+++ 07-biblioteca-de-componente/App.js
@@ -1,41 +1,82 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import { PaperProvider, Provider, Text } from "react-native-paper";
+import { FlatList, ScrollView, StyleSheet, View } from "react-native";
+import { Divider, PaperProvider, Provider, Text } from "react-native-paper";
 import { Button, Card, Title, Paragraph } from "react-native-paper";
 
 export default function App() {
+  const lista = ["Uva", "Pera", "Morango"];
+
+  const lista2 = [
+    {
+      titulo: "titulo qualquer",
+      descricao: "Vitin",
+      imagem:
+        "https://assets.goal.com/images/v3/blt9a8c3f531c89ae83/GOAL%20-%20Blank%20WEB%20-%20Facebook(1902).jpeg?auto=webp&format=pjpg&width=3840&quality=60",
+    },
+    {
+      titulo: "titulo qualquer",
+      descricao: "Vitin",
+      imagem:
+        "https://assets.goal.com/images/v3/blt9a8c3f531c89ae83/GOAL%20-%20Blank%20WEB%20-%20Facebook(1902).jpeg?auto=webp&format=pjpg&width=3840&quality=60",
+    },
+    {
+      titulo: "titulo qualquer",
+      descricao: "Vitin",
+      imagem:
+        "https://assets.goal.com/images/v3/blt9a8c3f531c89ae83/GOAL%20-%20Blank%20WEB%20-%20Facebook(1902).jpeg?auto=webp&format=pjpg&width=3840&quality=60",
+    },
+  ];
   return (
+    <ScrollView>
     <PaperProvider>
       <View style={styles.container}>
         <StatusBar style="auto" />
 
-     
+        <FlatList
+          horizontal
+          data={lista2}
+          renderItem={({ item }) => (
+            <Card.Content>
+              <Title>{item.titulo}</Title>
+              <Paragraph>{item.descricao}</Paragraph>
+              <Card.Cover
+                source={{
+                  uri: "https://assets.goal.com/images/v3/blt9a8c3f531c89ae83/GOAL%20-%20Blank%20WEB%20-%20Facebook(1902).jpeg?auto=webp&format=pjpg&width=3840&quality=60",
+                }}
+              ></Card.Cover>
+            </Card.Content>
+          )}
+        />
 
-        <Card>
-          <Card.Content>
-            <Title style={{ textAlign: "center", fontWeight:900 }}>Título</Title>
-            <Paragraph style={{marginBottom:10, fontWeight: 600 }}> Descrição: NEYMAR NEYMAR NEYMAR NEYMAR NEY
-               </Paragraph>
-            <Button mode="contained" style={{fontWeight:900 }}>Clique Aqui</Button>
-          </Card.Content>
-        </Card>
+        <FlatList
+          data={lista2}
+          renderItem={({ item }) => (
+            <Card.Content>
+              <Title>{item.titulo}</Title>
+              <Paragraph>{item.descricao}</Paragraph>
+              <Card.Cover
+                source={{
+                  uri: "https://assets.goal.com/images/v3/blt9a8c3f531c89ae83/GOAL%20-%20Blank%20WEB%20-%20Facebook(1902).jpeg?auto=webp&format=pjpg&width=3840&quality=60",
+                }}
+              ></Card.Cover>
+            </Card.Content>
+          )}
+        />
 
-        <Card style={{marginTop:10}} >
-          <Card.Content>
-            <Paragraph style={{marginBottom:10, fontWeight: 600}}>
-            <Text variant="displayMedium">
+        <FlatList data={lista} renderItem={({ item }) => <Text>{item}</Text>} />
 
-              NEY NEY NEY NEY
-              </Text>
-              </Paragraph>
-          </Card.Content>
-          <Card.Cover source={{ uri: 'https://assets.goal.com/images/v3/blt9a8c3f531c89ae83/GOAL%20-%20Blank%20WEB%20-%20Facebook(1902).jpeg?auto=webp&format=pjpg&width=3840&quality=60' }}></Card.Cover>
-        </Card>
-
-        
-
+        <FlatList
+          data={lista}
+          renderItem={({ item }) => (
+            <View>
+              <Text>{item}</Text>
+              <Divider />
+            </View>
+          )}
+        />
       </View>
     </PaperProvider>
+    </ScrollView>
   );
 }
 
---
+++ lista-times-jogadores/componentes/Time.jsx
@@ -0,0 +1,37 @@
+import { FlatList, StyleSheet, View } from 'react-native'
+import { Card, Text } from 'react-native-paper';
+import React from 'react'
+import Jogador from './Jogador';
+
+export default function Time(props) {
+
+const {nome, anoFundacao,mascote,imagem,jogadores} = props
+
+  return (
+    <Card style={{ margin: 10 }}>
+
+    <Card.Title title={nome}/>
+    <Card.Content>
+        <Text>{anoFundacao}</Text>
+        <Text>{mascote}</Text>
+    </Card.Content>
+    <Card.Cover source={{uri: imagem}}/>
+    <Card.Actions>
+        <FlatList 
+        horizontal
+        data={jogadores}
+        renderItem={({item}) => (
+          <Jogador
+          nome={item.nome}
+          numero={item.numero}
+          imagem={item.imagem}
+          />
+        )}
+        />
+    </Card.Actions>
+
+</Card>
+  )
+}
+
+const styles = StyleSheet.create({})
